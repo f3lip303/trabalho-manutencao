da5a9bed80e1ed1cfd642ffd374d46ca2636ed6e
felipelima <felipelima@felipe.felipelima>
2019-09-23 11:02:47 -0300

    Iniciando Projeto

---
+++ src/main/java/com/trabalho/manutencao/api/TrabalhoManutencaoApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.manutencao.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoManutencaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoManutencaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabalho/manutencao/api/TrabalhoManutencaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trabalho.manutencao.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrabalhoManutencaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
