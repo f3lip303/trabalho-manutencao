b9c64b13f837b800faf896a3f90319f2e1bbd094
felipelima <felipelima@felipe.felipelima>
2019-09-24 17:22:20 -0300

    Criando classe de entidade

--- src/main/java/com/trabalho/manutencao/api/entity/Rota.java
+++ src/main/java/com/trabalho/manutencao/api/entity/Rota.java
@@ -1,8 +1,76 @@
 package com.trabalho.manutencao.api.entity;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
 
 @Entity
-public class Rota {
+@Table(name = "rota")
+public class Rota implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3055046711718042703L;
+	// Mapeando Banco De Dados
+	@Id
+	@Column(nullable = false)
+	private String codrota;
+
+	@Column(nullable = false)
+	private String tempoDaRota;
+
+	@Column(nullable = false)
+	private Long capacidadeDaRota;
+
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "codrota")
+	@OrderBy("ordemnarotaupdated ASC")
+	private List<Visita> visitas;
+
+	public Rota() {
+
+	}
+
+	public String getCodrota() {
+		return codrota;
+	}
+
+	public void setCodrota(String codrota) {
+		this.codrota = codrota;
+	}
+
+	public String getTempoDaRota() {
+		return tempoDaRota;
+	}
+
+	public void setTempoDaRota(String tempoDaRota) {
+		this.tempoDaRota = tempoDaRota;
+	}
+
+	public Long getCapacidadeDaRota() {
+		return capacidadeDaRota;
+	}
+
+	public void setCapacidadeDaRota(Long capacidadeDaRota) {
+		this.capacidadeDaRota = capacidadeDaRota;
+	}
+
+	public List<Visita> getVisitas() {
+		return visitas;
+	}
+
+	public void setVisitas(List<Visita> visitas) {
+		this.visitas = visitas;
+	}
 
 }
--- src/main/java/com/trabalho/manutencao/api/entity/Visita.java
+++ src/main/java/com/trabalho/manutencao/api/entity/Visita.java
@@ -1,8 +1,205 @@
 package com.trabalho.manutencao.api.entity;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
-public class Visita {
+@Table(name = "visita")
+public class Visita implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 560701365606288572L;
+
+	@Id
+	private Long id;
+
+	@Column(nullable = false)
+	private String codrota;
+
+	@Column(nullable = false)
+	private Double ptx;
+	@Column(nullable = false)
+	private Double pty;
+
+	private Double distaproxvisitaplan;
+	private Integer ordemnarotaplan;
+	private Double tpinicioatendimentoplan;
+	private Double tpfimatendimentoplan;
+	private Double distaproxvisitaupdated;
+	private Integer ordemnarotaupdated;
+	private Double tpinicioatendimentoupdated;
+	private Double tpfimatendimentoupdated;
+	private Double distaproxvisitaexec;
+	private Integer ordemnarotaexec;
+	private Double tpinicioatendimentoexec;
+	private Double tpfimatendimentoexec;
+	private String segmentos;
+	private Double janeladeatendimentoini;
+	private Double janeladeatendimentofim;
+
+	public Visita() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodrota() {
+		return codrota;
+	}
+
+	public void setCodrota(String codrota) {
+		this.codrota = codrota;
+	}
+
+	public Double getPtx() {
+		return ptx;
+	}
+
+	public void setPtx(Double ptx) {
+		this.ptx = ptx;
+	}
+
+	public Double getPty() {
+		return pty;
+	}
+
+	public void setPty(Double pty) {
+		this.pty = pty;
+	}
+
+	public Double getDistaproxvisitaplan() {
+		return distaproxvisitaplan;
+	}
+
+	public void setDistaproxvisitaplan(Double distaproxvisitaplan) {
+		this.distaproxvisitaplan = distaproxvisitaplan;
+	}
+
+	public Integer getOrdemnarotaplan() {
+		return ordemnarotaplan;
+	}
+
+	public void setOrdemnarotaplan(Integer ordemnarotaplan) {
+		this.ordemnarotaplan = ordemnarotaplan;
+	}
+
+	public Double getTpinicioatendimentoplan() {
+		return tpinicioatendimentoplan;
+	}
+
+	public void setTpinicioatendimentoplan(Double tpinicioatendimentoplan) {
+		this.tpinicioatendimentoplan = tpinicioatendimentoplan;
+	}
+
+	public Double getTpfimatendimentoplan() {
+		return tpfimatendimentoplan;
+	}
+
+	public void setTpfimatendimentoplan(Double tpfimatendimentoplan) {
+		this.tpfimatendimentoplan = tpfimatendimentoplan;
+	}
+
+	public Double getDistaproxvisitaupdated() {
+		return distaproxvisitaupdated;
+	}
+
+	public void setDistaproxvisitaupdated(Double distaproxvisitaupdated) {
+		this.distaproxvisitaupdated = distaproxvisitaupdated;
+	}
+
+	public Integer getOrdemnarotaupdated() {
+		return ordemnarotaupdated;
+	}
+
+	public void setOrdemnarotaupdated(Integer ordemnarotaupdated) {
+		this.ordemnarotaupdated = ordemnarotaupdated;
+	}
+
+	public Double getTpinicioatendimentoupdated() {
+		return tpinicioatendimentoupdated;
+	}
+
+	public void setTpinicioatendimentoupdated(Double tpinicioatendimentoupdated) {
+		this.tpinicioatendimentoupdated = tpinicioatendimentoupdated;
+	}
+
+	public Double getTpfimatendimentoupdated() {
+		return tpfimatendimentoupdated;
+	}
+
+	public void setTpfimatendimentoupdated(Double tpfimatendimentoupdated) {
+		this.tpfimatendimentoupdated = tpfimatendimentoupdated;
+	}
+
+	public Double getDistaproxvisitaexec() {
+		return distaproxvisitaexec;
+	}
+
+	public void setDistaproxvisitaexec(Double distaproxvisitaexec) {
+		this.distaproxvisitaexec = distaproxvisitaexec;
+	}
+
+	public Integer getOrdemnarotaexec() {
+		return ordemnarotaexec;
+	}
+
+	public void setOrdemnarotaexec(Integer ordemnarotaexec) {
+		this.ordemnarotaexec = ordemnarotaexec;
+	}
+
+	public Double getTpinicioatendimentoexec() {
+		return tpinicioatendimentoexec;
+	}
+
+	public void setTpinicioatendimentoexec(Double tpinicioatendimentoexec) {
+		this.tpinicioatendimentoexec = tpinicioatendimentoexec;
+	}
+
+	public Double getTpfimatendimentoexec() {
+		return tpfimatendimentoexec;
+	}
+
+	public void setTpfimatendimentoexec(Double tpfimatendimentoexec) {
+		this.tpfimatendimentoexec = tpfimatendimentoexec;
+	}
+
+	public String getSegmentos() {
+		return segmentos;
+	}
+
+	public void setSegmentos(String segmentos) {
+		this.segmentos = segmentos;
+	}
+
+	public Double getJaneladeatendimentoini() {
+		return janeladeatendimentoini;
+	}
+
+	public void setJaneladeatendimentoini(Double janeladeatendimentoini) {
+		this.janeladeatendimentoini = janeladeatendimentoini;
+	}
+
+	public Double getJaneladeatendimentofim() {
+		return janeladeatendimentofim;
+	}
+
+	public void setJaneladeatendimentofim(Double janeladeatendimentofim) {
+		this.janeladeatendimentofim = janeladeatendimentofim;
+	}
 
 }
