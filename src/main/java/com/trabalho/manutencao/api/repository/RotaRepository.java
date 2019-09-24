f887db93ea2568c99bc9195e93673f09b8d2cb62
felipelima <felipelima@felipe.felipelima>
2019-09-24 17:22:07 -0300

    Criando interfaces de repositorio

---
+++ src/main/java/com/trabalho/manutencao/api/repository/RotaRepository.java
@@ -0,0 +1,9 @@
+package com.trabalho.manutencao.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trabalho.manutencao.api.entity.Rota;
+
+public interface RotaRepository extends JpaRepository<Rota, String>{
+
+}
---
+++ src/main/java/com/trabalho/manutencao/api/repository/VisitaRepository.java
@@ -0,0 +1,9 @@
+package com.trabalho.manutencao.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trabalho.manutencao.api.entity.Visita;
+
+public interface VisitaRepository extends JpaRepository<Visita, Long>{
+
+}
